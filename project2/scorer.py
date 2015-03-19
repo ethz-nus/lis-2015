628de775a7d741b7c137aabb1ee5997a4179fe0c
JishnuM <mohanjishnu@gmail.com>
2015-03-19 21:48:14 +0100

    Add scorer function

---
+++ project2/scorer.py
@@ -0,0 +1,18 @@
+
+def calculate_score(predicted_labels, actual_labels):
+    '''
+    Expects both inputs in format of list with elements as dicts as follows:
+    {
+        Y: predicted / actual label for Y
+        Z: predicted / actual label for Z
+    }
+    '''
+    yscore = 0.0
+    zscore = 0.0
+    n = len(predicted_labels)
+    for i in range(n):
+        if predicted_labels[i]['Y'] != actual_labels[i]['Y']:
+            yscore += 1.0
+        if predicted_labels[i]['Z'] != actual_labels[i]['Z']:
+            zscore += 1.0
+    return (yscore + zscore)/(2*n)
