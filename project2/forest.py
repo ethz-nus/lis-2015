5b93a3309a701fb5aba1ff831190600a6e747f66
Naomi Leow <naomileowwx@gmail.com>
2015-03-20 00:24:01 +0100

    added file with forest implementation

---
+++ project2/forest.py
@@ -0,0 +1,11 @@
+from sklearn.ensemble import *
+import sklearn.cross_validation as skcv
+
+def random_forest(X, Y):
+	Xtrain, Xtest, Ytrain, Ytest = skcv.train_test_split(X, Y, train_size=0.75)
+	trainer = RandomForestClassifier(n_jobs=-1)
+
+	classifier = trainer.fit(Xtrain, Ytrain)
+	Ypred = trainer.predict(Xtest)
+
+	return classifier, Ypred, Ytest
