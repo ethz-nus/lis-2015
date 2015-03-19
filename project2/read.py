335eed52a2be029781da65b7e4e69f564a683763
JishnuM <mohanjishnu@gmail.com>
2015-03-19 21:47:32 +0100

    Separate read functions for x and y variables

--- project2/read.py
+++ project2/read.py
@@ -1,6 +1,6 @@
 import csv
 
-def read_data(filepath):
+def read_x(filepath):
 	'''
 	Each row will be read into a dictionary with the format:
 	{
@@ -13,11 +13,27 @@ def read_data(filepath):
 	with open(filepath, 'r') as fin:
 		rows = csv.reader(fin, delimiter=',')
 		for row in rows:
-			vals = list(map(int, row[:10]))
-			kvals = list(map(int, row[10: 14]))
-			lvals = list(map(int, row[14:]))
+			vals = list(map(float, row[:10]))
+			kvals = list(map(float, row[10: 14]))
+			lvals = list(map(float, row[14:]))
 			allVals = dict(numeric=vals, catk=kvals, catl=lvals)
 			data.append(allVals)
 	return data
+
+def read_y(filepath):
+    '''
+    Each row will be read into a dict with the format:
+    {
+        Y: Numeric label in range 1-7
+        Z: Numeric label in range 0-2
+    }
+    '''
+    data = []
+    with open(filepath, 'r') as fin:
+        rows = csv.reader(fin, delimiter=',')
+        for row in rows:
+            rowVals = dict(Y=int(row[0]), Z=int(row[1]))
+            data.append(rowVals)
+    return data
 
 #data = read_data("project_data/train.csv")
