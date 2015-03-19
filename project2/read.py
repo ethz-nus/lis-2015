852ea60c783bfebc265c8865290629a1b15cab7e
Naomi Leow <naomileowwx@gmail.com>
2015-03-19 11:23:36 +0100

    method to read in formatted data from file

---
+++ project2/read.py
@@ -0,0 +1,23 @@
+import csv
+
+def read_data(filepath):
+	'''
+	Each row will be read into a dictionary with the format:
+	{
+		numeric: values of features A to I,
+		catk: list representing vector for categorical feature K,
+		catl: list representing vector for categorical feature L
+	}
+	'''
+	data = []
+	with open(filepath, 'r') as fin:
+		rows = csv.reader(fin, delimiter=',')
+		for row in rows:
+			vals = list(map(int, row[:10]))
+			kvals = list(map(int, row[10: 14]))
+			lvals = list(map(int, row[14:]))
+			allVals = dict(numeric=vals, catk=kvals, catl=lvals)
+			data.append(allVals)
+	return data
+
+#data = read_data("project_data/train.csv")
